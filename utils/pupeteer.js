cc015ef07acb66e0cafb0b219cd8f4942a401d7c
muhammad anfas <muhammedanfaskclm@gmail.com>
2026-05-21 19:32:28 +0530

    fixed the pupeteer issue

--- utils/pupeteer.js
+++ utils/pupeteer.js
@@ -1,4 +1,4 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { executablePath } from 'puppeteer';
 
 let browser;
 
@@ -6,6 +6,7 @@ export const getBrowser = async () => {
   if (!browser) {
     browser = await puppeteer.launch({
       headless: 'new',
+      executablePath : process.env.CHROME_PATH ,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
   }
