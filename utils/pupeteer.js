b38c86cacb48c78f803ba30dfde2ad0d2b702fc8
muhammad anfas <muhammedanfaskclm@gmail.com>
2026-05-23 13:48:00 +0530

    fixed the pupeeter error , and fixed the category dropdown selection

--- utils/pupeteer.js
+++ utils/pupeteer.js
@@ -4,7 +4,12 @@ import puppeteer, { executablePath } from 'puppeteer';
 export async function generatePdf(html) {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: [
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--single-process",
+  ],
   });
   try {
     const page = await browser.newPage();
